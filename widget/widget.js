2ef2477a43aeffd905ea54c85b441b2c1a6beb17
udawee <uisurikaw@gmail.com>
2023-08-08 14:49:32 +1000

    changes to widget

--- widget/widget.js
+++ widget/widget.js
@@ -1,35 +1,70 @@
 'use strict'
-// Write your module here
-// It must send an event "frames:loaded" from the top frame containing a list of { name:label } pairs,
-// which describes all the fields in each frame.
 
-// This is a template to help you get started, feel free to make your own solution.
 function execute() {
 	try {
-    // Step 1 Scrape Fields and Create Fields list object.
-    // Step 2 Add Listener for Top Frame to Receive Fields.
-    if (isTopFrame()) {
-      window.addEventListener('message', (event) => {
-        // - Merge fields from frames.
-        // - Process Fields and send event once all fields are collected.
-      });
-    } else if (!isTopFrame()) {
-      // Child frames sends Fields up to Top Frame.
-    }
+	    //Scrape Fields and Create Fields list object.
+	    let fields = scrapeFields();
+
+	    //Adding Listener for Top Frame to Receive Fields.
+	    if (isTopFrame()) {
+	        let expectedFrames = 2;
+	        let collectedFields = [...fields];
+
+	        window.addEventListener('message', (event) => {
+	            if (event.data && event.data.fields) {
+	                collectedFields.push(...event.data.fields);
+	            }
+
+	            expectedFrames--;
+
+	            if (expectedFrames === 0) {
+	                // Fields of all frames
+	                collectedFields.sort((a, b) => {
+	                    return Object.keys(a)[0].localeCompare(Object.keys(b)[0]);
+	                });
+
+	                const framesLoadedEvent = new CustomEvent('frames:loaded', {
+	                    detail: {
+	                        fields: collectedFields
+	                    }
+	                });
+	                document.dispatchEvent(framesLoadedEvent);
+	            }
+	        });
+
+	    } else {
+	        // Child frames sending fields to Top Frame.
+	        getTopFrame().postMessage({ fields: fields }, '*');
+	    }
 	} catch (e) {
-		console.error(e)
+	    console.error(e);
 	}
+}
+
+function scrapeFields() {
+    let fieldsList = [];
+    let formElements = document.querySelectorAll('input, select');
+
+    formElements.forEach(element => {
+        let name = element.name;
+        let label = document.querySelector(`label[for="${name}"]`);
+        if (label) {
+            let fieldObject = {};
+            fieldObject[name] = label.innerText.trim();
+            fieldsList.push(fieldObject);
+        }
+    });
+
+    return fieldsList;
 }
 
 execute();
 
-// Utility functions to check and get the top frame
-// as Karma test framework changes top & context frames.
-// Use this instead of "window.top".
+//getting the top frame.
 function getTopFrame() {
-  return window.top.frames[0];
+    return window.top.frames[0];
 }
 
 function isTopFrame() {
-  return window.location.pathname == '/context.html';
+    return window.location.pathname == '/context.html';
 }
